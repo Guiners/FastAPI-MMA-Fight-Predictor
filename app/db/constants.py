2f241804116b68174337fc2b55369ae51a1ec19e
Guiners <rkoza@softserveinc.com>
2025-09-25 19:50:52 +0200

    Updating docker and adding working alembic code

---
+++ app/db/constants.py
@@ -0,0 +1,11 @@
+from app.db.models.fighters import Fighters
+from app.db.models.base_stats import BaseStats
+from app.db.models.extended_stats import ExtendedStats
+from app.db.models.fights_results import FightsResults
+
+example_data_paths = {
+    Fighters: "./app/db/example_data/fighters.json",
+    BaseStats: "./app/db/example_data/base_stats.json",
+    ExtendedStats: "./app/db/example_data/extended_stats.json",
+    FightsResults: "./app/db/example_data/fights_results.json"
+}
---
+++ app/db/scripts/seed.py
@@ -0,0 +1,47 @@
+import json
+from app.db.database import get_db
+from sqlalchemy.orm import Session
+from pathlib import Path
+from app.db.constants import example_data_paths
+
+class TableFiller:
+    """Utility class for populating database tables with initial data from JSON files."""
+
+    @staticmethod
+    def _fill_table_from_json(database: Session, table, json_path: str) -> None:
+        """Populate a single database table from a JSON file.
+
+        Args:
+            database (Session): Active SQLAlchemy session.
+            table (Type): ORM model class representing the target table.
+            json_path (str): Path to the JSON file containing records.
+        """
+        path = Path(json_path)
+        if not path.exists():
+            raise FileNotFoundError(f"JSON file not found: {json_path}")
+
+        with path.open(encoding="utf-8") as file:
+            for data in json.load(file):
+                database.add(table(**data))
+
+    @staticmethod
+    def fill_database_with_data(database: Session, data_dict: str) -> None:
+        """Populate all database tables defined in `example_data_paths`.
+        This method iterates through the `example_data_paths` list and
+        inserts the data into each corresponding table.
+
+        Args:
+            database (Session): Active SQLAlchemy session.
+            data_dict (Sequence[Tuple[Type, str]]): A sequence of tuples
+                where each tuple contains:
+                    - ORM model class (table)
+                    - Path to the JSON file with data.
+        """
+        for table, path in data_dict:
+            TableFiller._fill_table_from_json(database, table, path)
+        database.commit()
+
+if __name__ == "__main__":
+    mma_db = next(get_db())
+    print(type(mma_db))
+    TableFiller.fill_database_with_data(mma_db, example_data_paths)
